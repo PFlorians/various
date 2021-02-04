6c1e5c01ea5c07443ceca636dfc1f67817805f8e
I514742 <patrik.florians@sap.com>
2021-02-04 17:24:27 +0100

    prototyping

--- src/differentiation/Main.java
+++ src/differentiation/Main.java
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String args[])
 	{
 		PolishParser p = new PolishParser();
-		p.parse("(+ x (+ x x))");
+	//	p.parse("(+ x (+ x x))");
+		p.parse("(cos (* 2 x))");
 		p.parse("(- (+ x x) x)");
 	}
 }
--- src/differentiation/PolishParser.java
+++ src/differentiation/PolishParser.java
@@ -14,27 +14,36 @@ public class PolishParser {
 	{
 		//String[] partial_expressions = expression.split("(?<![^\\s\\(\\)])([^\\s\\(\\)]+)");
 		String partial_expressions[] = expression.split("[\\s\\(\\)]");
+
+		
 		List<String> tmp_buffer = Arrays.asList(partial_expressions);
 		List<String> l = tmp_buffer.stream().filter(Pattern.compile("[^\\s\\(\\)]+").asPredicate()).collect(Collectors.toList());
+		arithmethicSimplifier(l);
 		
-		long literals = Pattern.compile("(?<!\\S)([a-zA-z]{1})").matcher(expression).results().count();
+		/*long literals = Pattern.compile("(?<!\\S)([a-zA-z]{1})").matcher(expression).results().count();
 		long functions = Pattern.compile("(?<=\\S)([a-zA-z]{1})").matcher(expression).results().count();
 		long signs = Pattern.compile("[+-/*]{1}").matcher(expression).results().count();
 		long constants = Pattern.compile("\\d{1}").matcher(expression).results().count();
-		
-		calculateDecisionMatrix(partial_expressions);
+	*/	
 	}
-	private String convertExpression(String expressions[])
+	private void arithmethicSimplifier(List<String> expressions)
 	{
-		int i, j;
-		for( i = 0; i < expressions.length ; i++ )
+		int i;
+		for( i = 0; i < expressions.size(); i++ )
+		{
+			switchingLevel1(expressions.get(i));
+		}
+	}
+	private void switchingLevel1(String expression)
+	{
+		if( Pattern.compile("[+-/*^]").matcher(expression).matches() )
 		{
 			
 		}
-		return "";
+		else if ( Pattern.compile("cos|sin|tan|cot|exp").matcher(expression).matches() )
+		{
+			
+		}
 	}
-	private void calculateDecisionMatrix(String expressions[])
-	{
-		
-	}
+	
 }
